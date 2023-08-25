2a8038a3c3b9b639b6de28801b092b3a057595b3
Hrishikesh Keware <107115840+nice-hrishikesh-keware@users.noreply.github.com>
2023-08-25 14:08:27 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -11,7 +11,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-09c72d5a7ce529ad5"
+  ami = "ami-0f409bae3775dc8e5"
   instance_type = "t2.micro"
   key_name = "hrishi-import"
 
